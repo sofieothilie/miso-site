6e8a8d70ffe785839e09256746ff8edc98a15e7d
sofieothilie <sofieothilie@gmail.com>
2026-08-13 20:38:16 +0200

    omits calls to database

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,10 @@ export default async function Home() {
   // const supabase = await createClient()
   // const { data: todos, error } = await supabase.from('todos').select()
 
-  if (error) {
-    return <p>Error: {error.message}</p>
-  }
+  // if (error) {
+  //   return <p>Error: {error.message}</p>
+  // }
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
